a7d5b8a0b3d50cd410b981b7687467e7130a9f84
mahnafdev <mahnafdev@gmail.com>
2026-04-18 11:40:39 +0600

    feat(middleware): create async-handler middleware

---
+++ src/app/middlewares/asyncHandler.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const asyncHandler =
+	(fn: RequestHandler) => async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await fn(req, res, next);
+		} catch (err) {
+			next(err);
+		}
+	};
